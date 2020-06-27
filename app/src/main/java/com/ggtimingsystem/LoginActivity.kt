337410eeee656d3a986c80c4fcd8f24bffec6f2e
Kent Facey <kentfacey@gmail.com>
2020-06-26 22:29:44 -0400

    rounded buttons

--- app/src/main/java/com/ggtimingsystem/LoginActivity.kt
+++ app/src/main/java/com/ggtimingsystem/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.ggtimingsystem
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.ggtimingsystem.Main.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -11,7 +14,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        need_an_account_text_view.setOnClickListener {
+        login_button_login.setOnClickListener {
+
+            performLogin()
+
+
+        }
+
+        need_an_account_text_view_login.setOnClickListener {
             Log.d("LoginActivity", "Try to show register activity")
 
             // Launch login activity
@@ -19,5 +29,33 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun performLogin() {
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText( this, "Email or password cannot be empty!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        // Firebase signIn
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if successful
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
+                startActivity(intent)
+                finish()
+
+            }
+            .addOnFailureListener {
+                Log.d("Login", "Failed to login: ${it.message}")
+                Toast.makeText( this, "Failed to login: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/ggtimingsystem/RegisterActivity.kt
+++ app/src/main/java/com/ggtimingsystem/RegisterActivity.kt
@@ -1,9 +1,18 @@
 package com.ggtimingsystem
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -12,20 +21,81 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         register_button_register.setOnClickListener {
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_register.text.toString()
-
-            Log.d("RegisterActivity", "Email is: $email")
-            Log.d("RegisterActivity", "Password:$password")
-
-            // Create user with username and password
+            performRegister()
         }
-        already_have_account_text_view.setOnClickListener {
+        already_have_account_text_view_register.setOnClickListener {
             Log.d("RegisterActivity", "Try to show login activity")
 
             // Go back to login activity
             finish()
         }
+        picture_button_register.setOnClickListener {
+            Log.d("RegisterActivity", "Try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    var pictureUri: Uri? = null
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            // photo has been selected
+            Log.d("Register", "Photo was selected")
+            pictureUri = data.data
+
+            //select photo from uri
+            val bitmap = when {
+                Build.VERSION.SDK_INT < 28 -> MediaStore.Images.Media.getBitmap(
+                    this.contentResolver,
+                    pictureUri
+                )
+                else -> {
+                    val source = ImageDecoder.createSource(this.contentResolver, pictureUri!!)
+                    ImageDecoder.decodeBitmap(source)
+                }
+            }
+
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            picture_button_register.setBackgroundDrawable(bitmapDrawable)
+        }
+    }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText( this, "Email or password cannot be empty!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("RegisterActivity", "Email is: $email")
+        Log.d("RegisterActivity", "Password:$password")
+
+        //Create user with Firebase Authentication
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if successful
+                Log.d("Register", "Created user with uid: ${it.result?.user?.uid}")
+                Toast.makeText( this, "Create user with email: $email", Toast.LENGTH_SHORT).show()
+                uploadImageToFirebaseSorage()
+
+            }
+            .addOnFailureListener {
+                Log.d("Register", "Failed to create user: ${it.message}")
+                Toast.makeText( this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun uploadImageToFirebaseSorage() {
 
     }
 }
